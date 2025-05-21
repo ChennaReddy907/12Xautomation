77d337669af02e750e271aa7b9c533117c01c4f3
ChennaReddy907 <chenna_reddy111@yahoo.com>
2025-05-21 13:17:18 +0530

    practice8 Class code updated

---
+++ src/Practice7_java.java
@@ -0,0 +1,10 @@
+public class Practice7_java {
+    public static void main (String[]args){
+
+        int num=81;
+
+        String result = ( num % 2 == 0 ) ? "even" : "odd" ;
+
+        System.out.println(result);
+    }
+}
