ca0b27416d34613dc4711e2475dd331ce1340a4b
ChennaReddy907 <chenna_reddy111@yahoo.com>
2025-06-19 00:41:04 +0530

    FindMaxOfTwoNumbers

---
+++ src/FindTheMaximumOfTwoNumbers.java
@@ -0,0 +1,16 @@
+public class FindTheMaximumOfTwoNumbers {
+    public static void main(String[] args){
+        int a=100;
+        int b=700;
+
+        if(a > b){
+            System.out.println("Max num is:"+a);
+        }
+        else if(b > a){
+            System.out.println("Max num is:"+b);
+        }
+        else{
+            System.out.println("Both are equal");
+        }
+    }
+}
