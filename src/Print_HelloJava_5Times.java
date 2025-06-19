34f7bea45903e124d01323603c699245ee76763b
ChennaReddy907 <chenna_reddy111@yahoo.com>
2025-06-19 15:04:03 +0530

    Print_HelloJava_5Times user while loop

---
+++ src/Print_HelloJava_5Times.java
@@ -0,0 +1,9 @@
+public class Print_HelloJava_5Times {
+    public static void main(String[] args){
+        int i=1;
+        while(i<=5){
+            System.out.println("Hello Java");
+            i++;
+        }
+    }
+}
