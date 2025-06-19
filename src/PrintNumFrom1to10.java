12f9d89af46b71fe65c64b1b303a131e54200280
ChennaReddy907 <chenna_reddy111@yahoo.com>
2025-06-19 11:49:30 +0530

    PrintNumFrom1to10

---
+++ src/PrintNumFrom1to10.java
@@ -0,0 +1,11 @@
+public class PrintNumFrom1to10 {
+    public static void main(String[] args){
+
+        int i =1;
+
+        for(;i<=10;i++){
+            System.out.println(i);
+        }
+
+    }
+}
