8d922344f733d464729c3423b400060142ec29fd
ChennaReddy907 <chenna_reddy111@yahoo.com>
2025-06-19 12:12:54 +0530

    Print_HELLO_5Times

---
+++ src/String_Logics/Print_HELLO_5Times.java
@@ -0,0 +1,9 @@
+package String_Logics;
+
+public class Print_HELLO_5Times {
+    public static void main(String[] args){
+        for(int i=1;i<=5;i++){
+            System.out.println("Hello");
+        }
+    }
+}
