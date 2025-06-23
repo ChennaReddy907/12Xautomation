f60d7e1a3c0cb3a545a278f2e08721479a8a428d
ChennaReddy907 <chenna_reddy111@yahoo.com>
2025-06-23 11:26:37 +0530

    UseWhileLooptoPrintTableofNumber

---
+++ src/UseWhileLooptoPrintTableofNumber.java
@@ -0,0 +1,11 @@
+public class UseWhileLooptoPrintTableofNumber {
+    public static void main(String[] args){
+        int i =1;
+        int num=8;
+
+        while(i<=10){
+            System.out.println(num + "X" + i + "=" + (num*i));
+            i++;
+        }
+    }
+}
