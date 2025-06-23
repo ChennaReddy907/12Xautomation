c830a3f6006e1a3a2b567d99348e0b9c124b7259
ChennaReddy907 <chenna_reddy111@yahoo.com>
2025-06-23 11:25:41 +0530

    DifferentofIncrement_Decrement

---
+++ src/DifferentofIncrement_Decrement.java
@@ -0,0 +1,14 @@
+public class DifferentofIncrement_Decrement {
+    public static void main(String[] args){
+
+        int a =5;
+        int b=6;
+        System.out.println(a);
+        System.out.println(++a);
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(b++);
+        System.out.println(b);
+
+    }
+}
