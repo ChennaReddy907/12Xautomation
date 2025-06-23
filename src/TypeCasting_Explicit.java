c98c887809bb04a7e21b12b7e804febc8edf2bf7
ChennaReddy907 <chenna_reddy111@yahoo.com>
2025-06-23 12:26:06 +0530

    TypeCasting_Explicit

---
+++ src/TypeCasting_Explicit.java
@@ -0,0 +1,10 @@
+public class TypeCasting_Explicit {
+    public static void main(String[] args){
+
+        double x =99.98;
+        int y=(int) x;
+
+        System.out.println("Original "+ x);
+        System.out.println("Explicit type casting from double to int  :" +y);
+    }
+}
