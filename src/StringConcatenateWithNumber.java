d3a18b1316b0c72dcaf6850b3e7f025ad230b878
ChennaReddy907 <chenna_reddy111@yahoo.com>
2025-06-23 11:25:04 +0530

    StringConcatenateWithNumber

---
+++ src/StringConcatenateWithNumber.java
@@ -0,0 +1,15 @@
+public class StringConcatenateWithNumber {
+
+    public static void main(String[] args){
+        System.out.println(10+20+"java");
+        System.out.println("Java"+10+30);
+        System.out.println(10+"java"+20);
+        System.out.println(10+'A'+"Tech");
+        System.out.println(50+"A"+20+30+"tech");
+        System.out.println("a"+"b");
+        System.out.println("100"+'A');
+        System.out.println("A"+ 20);
+
+
+    }
+}
