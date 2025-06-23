0f4bc07da13052bfaf41c04020b27f2290cd2bc8
ChennaReddy907 <chenna_reddy111@yahoo.com>
2025-06-23 12:39:01 +0530

    TypeCasting_Implicit

---
+++ src/TypeCasting_Implicit.java
@@ -0,0 +1,10 @@
+public class TypeCasting_Implicit {
+    public static void main(String[] args){
+        double x = 192.10;
+        int y= (int)x;
+
+        System.out.println("Original double x :"+ x);
+        System.out.println("Implicit Y: "+ y);
+
+    }
+}
