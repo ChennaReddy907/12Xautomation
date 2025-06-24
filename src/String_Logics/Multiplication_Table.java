423852659471bbeff399c2c02fb9df31481a7327
ChennaReddy907 <chenna_reddy111@yahoo.com>
2025-06-24 18:20:06 +0530

    Multiplication_Table

---
+++ src/String_Logics/Multiplication_Table.java
@@ -0,0 +1,15 @@
+package String_Logics;
+
+public class Multiplication_Table {
+    public static void main(String[] args){
+
+        for(int i=1;i<=10;i++){
+
+            for(int j=1;j<=8;j++){
+                System.out.print(j*i +"\t");
+            }
+            System.out.println();
+        }
+
+    }
+}
